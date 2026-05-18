4ff6a3dda44809423de9c33c10b5785f03d54e1a
Test User <test@example.com>
2026-05-17 23:42:47 -0300

    pthread terminado, y ejemplo con paralelizacion contraproducente

--- TP2/matrices-pthread-full-par.c
+++ TP2/matrices-pthread-full-par.c
@@ -20,8 +20,25 @@ typedef struct {
     int blocks_per_dim;
 } thread_args_t;
 
+typedef struct {
+    int thread_id;
+    int start_idx;
+    int end_idx;
+    double MaxA, MinA, PromA;
+    double MaxB, MinB, PromB;
+} stats_args_t;
+
+typedef struct {
+    int thread_id;
+    int start_idx;
+    int end_idx;
+    double constante;
+} scalar_args_t;
+
 void *matmulblksRowColColParallel(void *ptr);
 void *matmulblksRowColRowParallel(void *ptr);
+void *calculateStatsParallel(void *ptr);
+void *scalarMultiplyParallel(void *ptr);
 
 //Mejora el performance y aprovecha optimizaciones del compilador
 void blkmulRowColCol(double *ablk, double *bblk, double *cblk, int n, int bs);
@@ -45,6 +62,8 @@ int main(int argc, char*argv[]) {
     pthread_attr_t attr;
     pthread_t threads[t];
     thread_args_t args[t];
+    stats_args_t stats_args[t];
+    scalar_args_t scalar_args[t];
 
     bool print = false;
     print = BS == 8;
@@ -77,27 +96,44 @@ int main(int argc, char*argv[]) {
 
     timetick = dwalltime();
 
-    //acceso secuencial para calcular maximo, minimo y promedio de A y B
-    // Esto podria ser paralelizable
-    // Pero no le veo sentido al ser una carga de trabajo tan pequeña comparada con la multiplicación de matrices
-    // Sin mencionar que la sincronizacion seria mas costosa que el beneficio de paralelizar esta parte
+    //acceso paralelizado para calcular maximo, minimo y promedio de A y B
     PromA = PromB = 0.0;
     MaxA = MinA = a[0];
     MaxB = MinB = b[0];
-
-    for (i = 0; i < n*n; i++) {
-        double valA = a[i];
-        double valB = b[i];
-
-        if (valA > MaxA) MaxA = valA;
-        if (valA < MinA) MinA = valA;
-        PromA += valA;
-
-        if (valB > MaxB) MaxB = valB;
-        if (valB < MinB) MinB = valB;
-        PromB += valB;
+    
+    pthread_attr_init(&attr);
+    
+    // Distribución de elementos a threads para cálculo de stats
+    int elements_per_thread = (n*n) / t;
+    for (i = 0; i < t; i++) {
+        stats_args[i].thread_id = i;
+        stats_args[i].start_idx = i * elements_per_thread;
+        stats_args[i].end_idx = (i == t - 1) ? (n*n) : (i + 1) * elements_per_thread;
+        stats_args[i].MaxA = a[stats_args[i].start_idx];
+        stats_args[i].MinA = a[stats_args[i].start_idx];
+        stats_args[i].PromA = 0.0;
+        stats_args[i].MaxB = b[stats_args[i].start_idx];
+        stats_args[i].MinB = b[stats_args[i].start_idx];
+        stats_args[i].PromB = 0.0;
+        pthread_create(&threads[i], &attr, calculateStatsParallel, (void *) &stats_args[i]);
     }
-
+    
+    // Sincronizar y recopilar resultados
+    for (i = 0; i < t; i++) {
+        pthread_join(threads[i], NULL);
+    }
+    
+    // Reducción secuencial de resultados parciales
+    for (i = 0; i < t; i++) {
+        if (stats_args[i].MaxA > MaxA) MaxA = stats_args[i].MaxA;
+        if (stats_args[i].MinA < MinA) MinA = stats_args[i].MinA;
+        PromA += stats_args[i].PromA;
+        
+        if (stats_args[i].MaxB > MaxB) MaxB = stats_args[i].MaxB;
+        if (stats_args[i].MinB < MinB) MinB = stats_args[i].MinB;
+        PromB += stats_args[i].PromB;
+    }
+    
     PromA /= (n*n);
     PromB /= (n*n);
     
@@ -105,7 +141,7 @@ int main(int argc, char*argv[]) {
     //primero B x BT para obtener D con column major order
     //luego A x D para obtener C con row major order 
     //inicializar el atributo de los threads
-    pthread_attr_init(&attr);
+    
     // Distribución de filas de bloques a threads
     // blocks_per_dim: cantidad de bloques por dimensión (filas o columnas de bloques BS x BS)
     int blocks_per_dim = n / BS;
@@ -141,13 +177,19 @@ int main(int argc, char*argv[]) {
         pthread_join(threads[i], NULL);
     }
     //recorrido lineal dado que es una constante matematica por una matriz
-    
-    //Esto podria ser facilmente paralelizable
-    //Pero no le veo sentido al ser una carga de trabajo tan pequeña
     double constante = ((MaxA * MaxB) - (MinA * MinB)) / (PromA * PromB);
 
-    for (i = 0; i < n*n; i++) {
-        r[i] = constante * c[i];
+    // Multiplicación escalar paralelizada
+    for (i = 0; i < t; i++) {
+        scalar_args[i].thread_id = i;
+        scalar_args[i].start_idx = i * elements_per_thread;
+        scalar_args[i].end_idx = (i == t - 1) ? (n*n) : (i + 1) * elements_per_thread;
+        scalar_args[i].constante = constante;
+        pthread_create(&threads[i], &attr, scalarMultiplyParallel, (void *) &scalar_args[i]);
+    }
+    //sincronizo antes de continuar
+    for (i = 0; i < t; i++) {
+        pthread_join(threads[i], NULL);
     }
 
     workTime = dwalltime() - timetick;
@@ -197,49 +239,105 @@ void * matmulblksRowColColParallel(void *ptr) {
     // Implementación especializada para A row-major, B column-major, C column-major
     // Copiar argumentos localmente para evitar race condition
     thread_args_t args_local = *((thread_args_t *) ptr);
-    int start_block_row = (int)args_local.start_block_row;
-    int end_block_row = (int)args_local.end_block_row;
-    int n = (int)args_local.N;
-    int bs = BS;
-    int blocks_per_dim = (int)args_local.blocks_per_dim;
     
-    // Orden de bucles i,k,j optimiza caché: B[i,k] se carga una sola vez y se reutiliza para todos los j
+    int start_block_row = args_local.start_block_row;
+    int end_block_row = args_local.end_block_row;
+    int n = args_local.N;
+    int bs = BS;
+    int blocks_per_dim = args_local.blocks_per_dim;
+    
     for (int i_block = start_block_row; i_block < end_block_row; i_block++) {
         int block_i = i_block * bs;
-        int in = block_i * n;
+        // Orden i, k, j optimiza reutilización de caché: B[i,k] se carga y reutiliza para todos los j
         for (int k = 0; k < n; k += bs) {
             int kn = k * n;
             for (int j_block = 0; j_block < blocks_per_dim; j_block++) {
                 int block_j = j_block * bs;
+                int in = block_i * n;
                 blkmulRowColCol(&b[in + k], &b[kn + block_j*n], &d[block_i + block_j*n], n, bs);
             }
         }
     }
     pthread_exit(NULL);
 }
+
 void * matmulblksRowColRowParallel(void *ptr) {
     // Implementación especializada para A row-major, B col-major, C row-major
     // Copiar argumentos localmente para evitar race condition
     thread_args_t args_local = *((thread_args_t *) ptr);
     
-    int start_block_row = (int)args_local.start_block_row;
-    int end_block_row = (int)args_local.end_block_row;
-    int n = (int)args_local.N;
+    int start_block_row = args_local.start_block_row;
+    int end_block_row = args_local.end_block_row;
+    int n = args_local.N;
     int bs = BS;
-    int blocks_per_dim = (int)args_local.blocks_per_dim;
+    int blocks_per_dim = args_local.blocks_per_dim;
     
-    // Orden de bucles i,k,j optimiza caché: A[i,k] se carga una sola vez y se reutiliza para todos los j
     for (int i_block = start_block_row; i_block < end_block_row; i_block++) {
         int block_i = i_block * bs;
-        int in = block_i * n;
+        // Orden i, k, j optimiza reutilización de caché: A[i,k] se carga y reutiliza para todos los j
         for (int k = 0; k < n; k += bs) {
             int kn = k * n;
             for (int j_block = 0; j_block < blocks_per_dim; j_block++) {
                 int block_j = j_block * bs;
+                int in = block_i * n;
                 blkmulRowColRow(&a[in + k], &d[kn + block_j], &c[in + block_j], n, bs);
             }
         }
     }
+    pthread_exit(NULL);
+}
+
+void * calculateStatsParallel(void *ptr) {
+    // Cálculo paralelo de máximos, mínimos y promedios
+    // Cada thread procesa su rango de elementos y mantiene resultados locales
+    stats_args_t *args = (stats_args_t *) ptr;
+    int start_idx = args->start_idx;
+    int end_idx = args->end_idx;
+    
+    // Inicializar con el primer elemento del rango
+    double MaxA_local = a[start_idx];
+    double MinA_local = a[start_idx];
+    double PromA_local = 0.0;
+    double MaxB_local = b[start_idx];
+    double MinB_local = b[start_idx];
+    double PromB_local = 0.0;
+    
+    // Recorrer el rango asignado
+    for (int i = start_idx; i < end_idx; i++) {
+        double valA = a[i];
+        double valB = b[i];
+        
+        if (valA > MaxA_local) MaxA_local = valA;
+        if (valA < MinA_local) MinA_local = valA;
+        PromA_local += valA;
+        
+        if (valB > MaxB_local) MaxB_local = valB;
+        if (valB < MinB_local) MinB_local = valB;
+        PromB_local += valB;
+    }
+    
+    // Guardar resultados locales en la estructura
+    args->MaxA = MaxA_local;
+    args->MinA = MinA_local;
+    args->PromA = PromA_local;
+    args->MaxB = MaxB_local;
+    args->MinB = MinB_local;
+    args->PromB = PromB_local;
+    
+    pthread_exit(NULL);
+}
+
+void * scalarMultiplyParallel(void *ptr) {
+    // Multiplicación escalar paralelizada: r[i] = constante * c[i]
+    scalar_args_t *args = (scalar_args_t *) ptr;
+    int start_idx = args->start_idx;
+    int end_idx = args->end_idx;
+    double constante = args->constante;
+    
+    for (int i = start_idx; i < end_idx; i++) {
+        r[i] = constante * c[i];
+    }
+    
     pthread_exit(NULL);
 }
 
@@ -258,6 +356,7 @@ void blkmulRowColCol(double *ablk, double *bblk, double *cblk, int n, int bs) {
             }
     }
 }
+
 void blkmulRowColRow(double *ablk, double *bblk, double *cblk, int n, int bs) {
     // Implementación especializada para A row-major, B column-major, C row-major
     for (int i = 0; i < bs; i++) {
